cdce7a4aca638675ec74a9bed5288647082a281c
NeVajnoKak <156511620+NeVajnoKak@users.noreply.github.com>
2024-04-27 02:56:40 +0500

    add test version of Jwt Spring Maven

---
+++ src/main/java/com/example/testJWT/config/JwtService.java
@@ -0,0 +1,69 @@
+package com.example.testJWT.config;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import io.jsonwebtoken.*;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    private static final String SECRET_KEY = "V+NOFo/jfc49dQFl2RnmTkvAvTWGZPbxiDd0c2k2nWKPfaFksWbkq1WHBv36Ia5LHTRKNZETVYH8pUxhuyvkq+RCIhYV3qy3pqyd4q95Vo22x+fPHDRclGaBQJOETdEqKTJex8CPUZcqNjzpudWjjN6072nR5f20gBWbQlnj3YU=";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(),userDetails);
+    }
+    public String generateToken(
+            Map<String , Object> extraClaims,
+            UserDetails userDetails
+    ){
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSigningKey(),SignatureAlgorithm.HS256)
+                .compact();
+
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+    public boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+    private Date extractExpiration(String token){
+        return extractClaim(token,Claims::getExpiration);
+    }
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+    private Key getSigningKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
---
+++ src/main/java/com/example/testJWT/model/Role.java
@@ -0,0 +1,6 @@
+package com.example.testJWT.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
